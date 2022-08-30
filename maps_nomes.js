90c58e14face28625b0548c5cc36442302f2e574
lucasleitee <lukas_vit@hotmail.com>
2022-08-30 17:39:50 -0300

    aula 4 e 5

---
+++ maps_nomes.js
@@ -0,0 +1,6 @@
+let nomes = ["ana Julia", "caio vinicius", "BIA silva"]
+
+const nomesAtualizados = nomes.map(nome => nome.toUpperCase())
+
+console.log(nomesAtualizados)
+
---
+++ reprovados.js
@@ -0,0 +1,5 @@
+const nomes = ['Ana', 'Marcos', 'Maria', 'Mauro']
+const notas = [7,4.5,8,7.5]
+
+const reprovados = nomes.filter( (aluno,i) => notas[i] < 5)
+console.log(`reprovador: ${reprovados}`)
